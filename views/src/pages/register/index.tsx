4a41556fd34e64c10107aae192a06fb76df0991b
Chandra Halim <chan.halim98@gmail.com>
2024-10-12 15:30:52 +0700

    add some feature

--- views/src/pages/register/index.tsx
+++ views/src/pages/register/index.tsx
@@ -1,12 +1,11 @@
 import { Inter } from "next/font/google";
-import Login from "@/components/Login";
-
+import Register from "../register";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className={inter.className}>
-      <Login />
+      <Register />
     </main>
   );
 }
